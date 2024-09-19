9dc14d9f71c277a7f4e81108fad3d60d3c6fc70d
Hassan Amin <145911534+HassanAmin-67@users.noreply.github.com>
2024-09-19 11:08:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("learnMoreButton").addEventListener("click", function() {
+        alert("Thanks for exploring Web Central! Stay tuned for more.");
+    });
+});
+
+/*
+document.getElementById("learnMoreButton").addEventListener("click", function() {
+    alert("Thanks for exploring Web Central! Stay tuned for more.");
+});
+*/
